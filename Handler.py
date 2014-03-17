8ca22bc0b3c606ec830c3e3280e3528cf2b251e6
bhirsch42 <benhirsch42@gmail.com>
2014-03-17 06:02:56 -0400

    Login and User access finished (I think)

--- Handler.py
+++ Handler.py
@@ -3,8 +3,8 @@ import jinja2
 import os
 import logging
 import time
-from google.appengine.ext import ndb
-# import UserDatabase
+from google.appengine.api import users
+import Database
 
 class Handler(webapp2.RequestHandler):
 
@@ -13,11 +13,6 @@ class Handler(webapp2.RequestHandler):
 		# Initialize jinja2 environment
 		self.template_dir = os.path.join(os.path.dirname(__file__), 'html')
 		self.jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(self.template_dir), autoescape=True)
-
-		# # Check if user is signed in
-		# self.user_cookie_name = 'user_id'
-		# uid = self.readSecureCookie(self.user_cookie_name)
-		# self.user = uid and UserDatabase.getByUsername(uid)
 
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
@@ -29,17 +24,21 @@ class Handler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
+	def get_google_user(self):
+		return users.get_current_user()
 
-	def setSecureCookie(self, name, val, days = None):
-		cookie_val = makeSecureVal(str(val))
-		if days == None:
-			self.response.headers.add_header('Set-Cookie', '%s=%s; Path=/' % (name, cookie_val))
-		else:
-			expiration = datetime.datetime.now() + datetime.timedelta(days = days)
-			self.response.headers.add_header('Set-Cookie', '%s=%s; expires=%s; Path=/;' % (name, cookie_val, expiration.strftime("%a, %d-%b-%Y %H:%M:%S GMT")))
+	def get_user(self):
+		return Database.get_user_from_google_user(self.get_google_user())
 
+	# def setSecureCookie(self, name, val, days = None):
+	# 	cookie_val = makeSecureVal(str(val))
+	# 	if days == None:
+	# 		self.response.headers.add_header('Set-Cookie', '%s=%s; Path=/' % (name, cookie_val))
+	# 	else:
+	# 		expiration = datetime.datetime.now() + datetime.timedelta(days = days)
+	# 		self.response.headers.add_header('Set-Cookie', '%s=%s; expires=%s; Path=/;' % (name, cookie_val, expiration.strftime("%a, %d-%b-%Y %H:%M:%S GMT")))
 
-	def readSecureCookie(self, name):
-		cookie_val = self.request.cookies.get(name)
-		if cookie_val and checkSecureVal(cookie_val):
-			return cookie_val.split('|')[0]
+	# def readSecureCookie(self, name):
+	# 	cookie_val = self.request.cookies.get(name)
+	# 	if cookie_val and checkSecureVal(cookie_val):
+	# 		return cookie_val.split('|')[0]
--- main.py
+++ main.py
@@ -2,60 +2,79 @@
 #
 # Copyright 2007 Google Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
 import webapp2
 from Handler import *
-from google.appengine.api import users
-from UserDatabase import *
+import Database
 
 class MainHandler(Handler):
 	def get(self):
-		self.render("home.html")
+		self.render('home.html', user=self.get_user())
 
 class LoginHandler(Handler):
 	def get(self):
 		# Login to Google
-		user = users.get_current_user()
+		user = self.get_google_user()
 		if not user:
 			self.redirect(users.create_login_url('/login'))
 			return
 
-		if not is_registered_user(user):
-			self.render("login_user_doesnt_exist.html")
+		if not Database.is_registered_user(user):
+			self.render('login_user_doesnt_exist.html', user=self.get_user())
+			return
 
-# IN PROGRESS.  STOPPED WORKING HERE
+		self.redirect('/')
+
+
 class CreateAccountHandler(Handler):
 	def get(self):
-		self.render("create_user.html")
+		if Database.is_registered_user(self.get_google_user()):
+			self.render('user_already_exists.html', user=self.get_user())
+		else:
+			self.render('create_user.html', user=self.get_user())
 
 	def post(self):
-		first_name = self.request.get("first_name")
-		last_name = self.request.get("last_name")
-		if first_name == '' or last_name == '':
-			self.render('create_user.html', error=True)
+		first_name = self.request.get('first_name')
+		last_name = self.request.get('last_name')
+		user = self.get_google_user();
+		if not user:
+			self.redirect('/login')
+		elif first_name == '' or last_name == '':
+			self.render('create_user.html', user=self.get_user(), error=True)
 		else:
-			UserDatabase.addUser(first_name, last_name, user.user_id())
-			self.render('user_created.html')
+			Database.addUser(user.user_id(), first_name, last_name)
+			self.render('user_created.html', user=self.get_user())
 
-# SHOULD MOVE SOMEWHERE LOGICAL
-def is_registered_user(user):
-	registered_users = ndb.gql("SELECT * FROM MyUser")
-	user_ids = [registered_user.google_user_id for registered_user in registered_users]
-	user_exists = user.user_id() in user_ids
+class LogoutHandler(Handler):
+	def get(self):
+		user = self.get_google_user()
+		if not user:
+			self.redirect('/')
+		self.redirect(users.create_logout_url('/'))
+
+class AdminHandler(Handler):
+	def get(self):
+		user = self.get_user()
+		if user.is_a_moderator
+			self.render('moderator.html', user=user)
+			return
+		self.render('permission_denied.html', user=user)
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler),
 	('/login', LoginHandler),
-	('/login/createaccount', CreateAccountHandler)
+	('/login/createaccount', CreateAccountHandler),
+	('/logout', LogoutHandler),
+	('/admin', AdminHandler)
 ], debug=True)
